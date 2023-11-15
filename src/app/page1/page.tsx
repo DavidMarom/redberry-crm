0d9806ff18b756564df0cace3014fea3a24a10b6
David Marom <davidma@matrix.co.il>
2023-11-15 09:36:15 +0200

    wip

---
+++ src/app/page1/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Welcome to my Next.js page!</h1>
+            <p>This is a simple example of a Next.js page.</p>
+        </div>
+    );
+};
+
+export default HomePage;
