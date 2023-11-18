0077a36869c1381034af756b8803cef416106f30
David Marom <davidma@matrix.co.il>
2023-11-18 19:27:45 +0200

    img login

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,22 +3,48 @@
 import React, { useEffect, useState } from 'react'
 import { Inter } from 'next/font/google'
 import { Sidebar, Card01 } from '@/components'
-import { Button } from '@mui/material';
+import { gprovider } from '@/services/firebase-config';
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
 import Image from 'next/image'
 import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const [loggedin, setLoggedin] = useState(false);
+  const [img, setImg] = useState('');
   const [show, setShow] = useState(true);
 
   useEffect(() => {
-    document.title = "Berry";
+    document.title = "Redberry CRM";
 
-    if (localStorage.getItem('loggedin') === 'yes') {
-      setLoggedin(true);
-    }
+    if (localStorage.getItem('loggedin') === 'yes') { setLoggedin(true) }
+    setImg(localStorage.getItem('img') ?? '');
   }, []);
+
+  const doSignOut = () => {
+    const auth = getAuth();
+    signOut(auth).then((res) => {
+      console.log(res);
+
+    }).catch((error) => { console.log(error) });
+  }
+
+  async function doSignup() {
+    const auth = getAuth();
+    signInWithPopup(auth, gprovider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken ?? null;
+        const user = result.user;
+        let usr = { name: user.displayName, mail: user.email, photoURL: user.photoURL, uid: user.uid, token: token };
+
+        setImg(usr.photoURL ?? '');
+        localStorage.setItem('img', usr.photoURL ?? '');
+        localStorage.setItem('loggedin', 'yes');
+        window.location.reload();
+
+      }).catch((error) => { console.log(error) });
+  }
 
   if (loggedin) {
 
@@ -26,13 +52,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <html lang="en">
         <body className={inter.className}>
           <div className="header" >
-            <form>
-              <button onClick={() => {
-                localStorage.setItem('loggedin', 'no');
-                window.location.reload();
-              }
-              }>Logout</button>
-            </form>
+
+            <button onClick={() => {
+              doSignOut();
+              localStorage.setItem('loggedin', 'no');
+              window.location.reload();
+            }
+            }>Logout</button>
+
+            <Image src={img} alt="Profile" width={40} height={24} priority />
           </div>
 
           <div className='page-container'>
@@ -48,35 +76,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   else {
     return (
       <html lang="en">
-
         <body className={inter.className}>
           <div className='page-container'>
             <div className="row">
-
               {show &&
                 <Card01 height='400px'>
-                  <div className='row'>
-                    <Image src="/lichi.svg" alt="Lichi Logo" width={100} height={24} priority />
-                  </div>
-                  <form>
-
-                    <div className='row'><Button color="primary"
-                      onClick={() => {
-                        setShow(false);
-
-                        localStorage.setItem('loggedin', 'yes');
-                        window.location.reload();
-                      }
-                      }>
-                      Login
-                    </Button>
-                    </div>
-
-                  </form>
+                  <div className='row'><Image src="/lichi.svg" alt="Lichi Logo" width={100} height={24} priority /></div>
+                  <button onClick={doSignup}>Login with google</button>
                 </Card01>
               }
             </div>
-
           </div>
         </body>
       </html>
