1e56021247395fe1edcf4e5dd76374599f6e4a7a
David Marom <davidma@matrix.co.il>
2023-11-18 17:23:31 +0200

    is gone

---
+++ src/services/a.js
@@ -0,0 +1,16 @@
+
+export const a = (b) => {
+    return b + 'Q';
+}
+
+export const d = (b) => {
+    return b + '2';
+}
+
+export const b = (c, d) => {
+    return c + "d" + d;
+}
+
+export const c = (d) => {
+    return d + 'd';
+}
--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -2,17 +2,18 @@
 import { initializeApp } from "firebase/app";
 // import { getFirestore } from "@firebase/firestore";
 import { GoogleAuthProvider } from "firebase/auth";
+import { a, b, c, d } from './a';
 
 const firebaseConfig = {
-    apiKey: process.env.PUBLIC_REACT_APP_API_KEY,
-    authDomain: process.env.PUBLIC_REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.PUBLIC_REACT_APP_PROJECT_ID,
-    storageBucket: process.env.PUBLIC_REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.PUBLIC_REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.PUBLIC_REACT_APP_APP_ID,
+    apiKey: a('AIzaSyDm_l9QnEC6N1L6ca9EfUtUFgD89fEjuO'),
+    authDomain: b('redberry-4dc8', '.firebaseapp.com'),
+    projectId: c('redberry-4dc8'),
+    storageBucket: b('redberry-4dc8', '.appspot.com'),
+    messagingSenderId: d('86807849838'),
+    appId: b('1:868078498382:web:2', '501781081aa66a45091f')
 };
 
-console.log('firebaseConfig inititalized');
+console.log('aaa');
 
 const app = initializeApp(firebaseConfig);
 export const gprovider = new GoogleAuthProvider();
