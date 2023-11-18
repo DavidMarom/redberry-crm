68d62bc266bb1c97bd4ca376795388b0ab029447
David Marom <davidma@matrix.co.il>
2023-11-18 16:04:51 +0200

    google login init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,39 @@
-// "use client"
-import Image from 'next/image'
+"use client"
+import { gprovider } from '@/services/firebase-config';
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
 
 export default function Home() {
+
+
+
+  async function doSignup() {
+    const auth = getAuth();
+    signInWithPopup(auth, gprovider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken ?? null;
+        const user = result.user;
+        let usr = {
+          name: user.displayName,
+          mail: user.email,
+          photoURL: user.photoURL,
+          uid: user.uid,
+          token: token
+        }
+
+        console.log(usr);
+
+      }).catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <div>
       <h1>Overview</h1>
       <p>TBD: Dashboard</p>
+      <button onClick={doSignup}>Login</button>
+
     </div>
   )
 }
---
+++ src/services/firebase-config.js
@@ -0,0 +1,19 @@
+'use client';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+import { GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: process.env.PUBLIC_REACT_APP_API_KEY,
+    authDomain: process.env.PUBLIC_REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.PUBLIC_REACT_APP_PROJECT_ID,
+    storageBucket: process.env.PUBLIC_REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.PUBLIC_REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.PUBLIC_REACT_APP_APP_ID,
+};
+
+console.log(firebaseConfig);
+
+const app = initializeApp(firebaseConfig);
+export const gprovider = new GoogleAuthProvider();
+export const db = getFirestore(app);
