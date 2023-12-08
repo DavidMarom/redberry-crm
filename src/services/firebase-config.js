f31b37ac9690cfe2d6cefef75dad1648b243c8b5
David Marom <davidmarom.pro@gmail.com>
2023-12-08 22:08:17 +0200

    fix

--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -4,15 +4,28 @@ import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider } from "firebase/auth";
 import { a, b, c, d } from './a';
 
+
 const firebaseConfig = {
-    apiKey: a('AIzaSyDm_l9QnEC6N1L6ca9EfUtUFgD89fEjuO'),
-    authDomain: b('redberry-4dc8', '.firebaseapp.com'),
-    projectId: c('redberry-4dc8'),
-    storageBucket: b('redberry-4dc8', '.appspot.com'),
-    messagingSenderId: d('86807849838'),
-    appId: b('1:868078498382:web:2', '501781081aa66a45091f')
+    // apiKey: a('AIzaSyDm_l9QnEC6N1L6ca9EfUtUFgD89fEjuO'),
+    apiKey: process.env.PUBLIC_REACT_APP_API_KEY,
+
+    // authDomain: b('redberry-4dc8', '.firebaseapp.com'),
+    authDomain: process.env.PUBLIC_REACT_APP_AUTH_DOMAIN,
+
+    // projectId: c('redberry-4dc8'),
+    projectId: process.env.PUBLIC_REACT_APP_PROJECT_ID,
+
+    // storageBucket: b('redberry-4dc8', '.appspot.com'),
+    storageBucket: process.env.PUBLIC_REACT_APP_STORAGE_BUCKET,
+
+    // messagingSenderId: d('86807849838'),
+    messagingSenderId: process.env.PUBLIC_REACT_APP_MESSAGING_SENDER_ID,
+
+    // appId: b('1:868078498382:web:2', '501781081aa66a45091f')
+    appId: process.env.PUBLIC_REACT_APP_APP_ID,
+
 };
 
-const app = initializeApp(firebaseConfig);
 export const gprovider = new GoogleAuthProvider();
+// const app = initializeApp(firebaseConfig);
 // export const db = getFirestore(app);
