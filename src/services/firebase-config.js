85efbd121b81d0e0717a56f2beae19baf3d78baf
David Marom <davidmarom.pro@gmail.com>
2023-12-10 11:39:43 +0200

    test14

--- src/services/firebase-config.js
+++ src/services/firebase-config.js
@@ -1,26 +1,13 @@
-'use client';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 import { GoogleAuthProvider } from "firebase/auth";
-import { a, b, c, d } from './a';
 
 const firebaseConfig = {
-    // apiKey: a('AIzaSyDm_l9QnEC6N1L6ca9EfUtUFgD89fEjuO'),
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-
-    // authDomain: b('redberry-4dc8', '.firebaseapp.com'),
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-
-    // projectId: c('redberry-4dc8'),
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-
-    // storageBucket: b('redberry-4dc8', '.appspot.com'),
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-
-    // messagingSenderId: d('86807849838'),
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-
-    // appId: b('1:868078498382:web:2', '501781081aa66a45091f')
     appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
