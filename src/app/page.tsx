2ca3fc4eedbf629ffefc8965d90a8a4844f0b556
David Marom <davidmarom.pro@gmail.com>
2023-12-14 21:08:00 +0200

    working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 "use client"
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Card01 } from '@/components';
 import PieChart from './PieChart';
 
 export default function Home() {
-
-  const [contacts, setContacts] = useState([]);
-  const [pieData, setPieData] = useState([0, 0, 0]);
 
   const countStatus = (contacts: any) => {
     let statusCount = [0, 0, 0];
@@ -22,19 +19,12 @@ export default function Home() {
     return statusCount;
   }
 
-  useEffect(() => {
-    const contacts = localStorage.getItem('contacts');
-    
-      setPieData(countStatus(JSON.parse(contacts ?? '')))
-    
-  }, []);
-
   return (
     <div>
       <h1>Overview</h1>
       <br />
       <Card01>
-        <PieChart countData={pieData}/>
+        <PieChart countData={countStatus(JSON.parse(localStorage.getItem('contacts') ?? ''))}/>
       </Card01>
     </div>
   )
