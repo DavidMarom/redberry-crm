d1a7482e0d795f4ff3e86093bd06e0cb1b4748aa
David Marom <davidmarom.pro@gmail.com>
2023-12-15 22:19:44 +0200

    modified side bar

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -7,7 +7,7 @@ function SideBar() {
         <div className={styles.container}>
             <Link href="/"><div className={styles.sidebarbtn}>Overview</div></Link>
             <Link href="/Contacts"><div className={styles.sidebarbtn}>Contacts</div></Link>
-            <Link href="/Mailing"><div className={styles.sidebarbtn}>Mailing</div></Link>
+            <Link href="/Notes"><div className={styles.sidebarbtn}>Notes</div></Link>
             <Link href="/About"><div className={styles.sidebarbtn}>Developers</div></Link>
         </div>
     )
