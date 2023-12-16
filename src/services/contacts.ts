2a45ccd919c7b4a7ef1facad2fc1c20fcf6296c8
David Marom <davidmarom.pro@gmail.com>
2023-12-16 22:11:18 +0200

    added fetch abuse protection

--- src/services/contacts.ts
+++ src/services/contacts.ts
@@ -1,17 +1,5 @@
 import http from '@/services/http';
 
-// export function getContactsByOwner(ownerId: string) {
-//     http.get(`contacts/${ownerId}`)
-//         .then((response: any) => {
-//             if (!response.data) { alert("No contacts found") }
-//             else {
-//                 setContacts(response.data);
-//                 localStorage.setItem("contacts", JSON.stringify(response.data));
-//             }
-//         })
-//         .catch((error: any) => { console.log(error) })
-// }
-
 export function getContactsByOwner(ownerId: string) {
     return http.get(`contacts/${ownerId}`)
         .then((response: any) => response.data)
