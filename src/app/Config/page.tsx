2a34f671322e186df442a58695b35e11b932d9bf
David Marom <davidmarom.pro@gmail.com>
2023-12-23 09:05:21 +0200

    mail page ready

--- src/app/Config/page.tsx
+++ src/app/Config/page.tsx
@@ -1,22 +1,13 @@
 "use client";
-
 import React from 'react';
-import { Card01 } from '@/components';
-import Link from 'next/link';
 
 const AboutPage = () => {
     return (
         <div>
             <h1>Config</h1>
             <br />
-            <h2>Your business details:</h2>
-            <p>Name:</p>
-            <p>Phone number:</p>
-            <p>Address:</p>
-            
-            <br />
-            <h2>Preferences:</h2>
-            <p>Allow beta features:</p>
+            <p>This page is coming soon...</p>
+            <p>Here you will be able to config your business details</p>
 
         </div>
     );
