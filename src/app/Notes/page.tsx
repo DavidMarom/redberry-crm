4e54aaa4b77ef448f852cb2784520d1aacb67b4c
David Marom <davidmarom.pro@gmail.com>
2024-01-01 08:23:09 +0200

    using react query

--- src/app/Notes/page.tsx
+++ src/app/Notes/page.tsx
@@ -1,32 +1,18 @@
 "use client";
 
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { getNotesByOwner, addNote, deleteNote } from "../../services/notes";
-import { dataExpired, updateLastFetch, setToStorage, getFromStorage } from '@/utils/utils';
 import { useQuery } from "react-query";
 import Image from "next/image";
 
 const NotesPage = () => {
-    const { data, isLoading, error } = useQuery("notes", () => getNotesByOwner(uid));
+    const { data, isLoading, error } = useQuery("notes", () => getNotesByOwner(uid), { staleTime: 300000 });
+
     const [notes, setNotes] = useState(data ?? []);
     const [loading, setLoading] = useState(false);
     const [input, setInput] = useState("");
     const user = localStorage.getItem("user");
     const uid = user ? JSON.parse(user).uid : null;
-
-
-    // useEffect(() => {
-    //     if (getFromStorage("notes")) { getFromStorage("notes") }
-
-    //     if (dataExpired() || !getFromStorage("notes")) {
-    //         updateLastFetch();
-    //         getNotesByOwner(uid).then((response: any) => {
-    //             setNotes(response);
-    //             setToStorage("notes", response);
-    //             localStorage.setItem("notes", JSON.stringify(response));
-    //         });
-    //     }
-    // }, []);
 
     const handleDelete = (id: string) => {
         setLoading(true);
@@ -55,32 +41,31 @@ const NotesPage = () => {
             })
             .catch((err) => { console.log(err) });
     }
-    if (!isLoading) {
-        return (
-            <div className='full-width'>
-                {isLoading ? <h1>Loading...</h1> : <h1>Notes</h1>}
-                <div className='grid-container '>
-                    <div className='grid-item'>
-                        <textarea onChange={handleChange} value={input} />
-                        <div className='row-r'>
-                            <button onClick={submitHandler}>
-                                <Image src="/enter.svg" alt="Enter" width={22} height={22} />
-                            </button>
-                        </div>
+
+    return (
+        <div className='full-width'>
+            {isLoading ? <h1>Loading...</h1> : <h1>Notes</h1>}
+            <div className='grid-container '>
+                <div className='grid-item'>
+                    <textarea onChange={handleChange} value={input} />
+                    <div className='row-r'>
+                        <button onClick={submitHandler}>
+                            <Image src="/enter.svg" alt="Enter" width={22} height={22} />
+                        </button>
                     </div>
-                    {
-                        notes.map((note: any) => (
-                            <div className='grid-item' key={note._id}>
-                                <button className="row-r" onClick={() => { handleDelete(note._id) }}><Image src="/x.svg" alt="Lichi Logo" width={18} height={18} />
-                                </button>
-                                <div>{note.text}</div>
-                            </div>
-                        ))
-                    }
                 </div>
-            </div >
-        );
-    }
+                {
+                    data && data.map((note: any) => (
+                        <div className='grid-item' key={note._id}>
+                            <button className="row-r" onClick={() => { handleDelete(note._id) }}><Image src="/x.svg" alt="Lichi Logo" width={18} height={18} />
+                            </button>
+                            <div>{note.text}</div>
+                        </div>
+                    ))
+                }
+            </div>
+        </div >
+    );
 };
 
 export default NotesPage;
