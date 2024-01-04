c76c4775205cec203805b0f5d3bb2ab32e554153
David Marom <davidmarom.pro@gmail.com>
2024-01-04 22:25:28 +0200

    added form

--- src/app/Config/page.tsx
+++ src/app/Config/page.tsx
@@ -1,19 +1,36 @@
 "use client";
 import React from 'react';
+import { Select, SelectItem, Button, Input } from "@nextui-org/react";
+import { Card01 } from "@/components";
+
+const sendHandler = (event: any) => {
+    event.preventDefault();
+    const data = new FormData(event.target);
+    const value = Object.fromEntries(data.entries());
+
+    console.log(value);
+
+}
 
 const AboutPage = () => {
     return (
         <div>
             <h1>Config</h1>
-            <br />
-            <p>This page is coming soon...</p>
-            <p>Here you will be able to config your business details</p>
-
-            <br />
-            <br />
-            <br />
-            v1.2.2
-
+            <form onSubmit={sendHandler}>
+                <Card01 width={"400px"} height="300px" justifycontent="space-between">
+                    <Input type="text" name="name" id="bizName" placeholder='Your business name' />
+                    <Select label="Country" className="max-w-xs" id="country" name="country">
+                        <SelectItem value="Israel" key={'Israel'}>Israel</SelectItem>
+                        <SelectItem value="USA" key={'USA'}>USA</SelectItem>
+                        <SelectItem value="UK" key={'UK'}>UK</SelectItem>
+                        <SelectItem value="Spain" key={'Spain'}>Spain</SelectItem>
+                        <SelectItem value="France" key={'France'}>France</SelectItem>
+                        <SelectItem value="Italy" key={'Italy'}>Italy</SelectItem>
+                        <SelectItem value="Germany" key={'Germany'}>Germany</SelectItem>
+                    </Select>
+                    <Button type="submit" color="primary">Save</Button>
+                </Card01>
+            </form>
         </div>
     );
 };
--- src/components/Card01/Card01.jsx
+++ src/components/Card01/Card01.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from "prop-types"
-import { Container, Column } from "./Card01.style"
+import { Container } from "./Card01.style"
 import { StyleSheetManager } from 'styled-components';
 
 export default function Card01({
@@ -10,20 +10,20 @@ export default function Card01({
     margintop = "20px",
     justifycontent,
     children,
+    backgroundColor = "#fff"
 }) {
 
     return (
-        <StyleSheetManager shouldForwardProp={(prop) => prop !== 'margintop' || prop !== 'marginright'}>
+        <StyleSheetManager shouldForwardProp={(prop) => prop !== 'margintop' || prop !== 'marginright' || prop !== 'justifycontent' || prop !== 'backgroundColor'}>
             <Container
                 width={width}
                 height={height}
                 margintop={margintop}
                 marginright={marginright}
                 justifycontent={justifycontent}
+                backgroundColor={backgroundColor}
             >
-                <Column>
-                    {children}
-                </Column>
+                {children}
             </Container>
         </StyleSheetManager>
     )
@@ -36,4 +36,5 @@ Card01.propTypes = {
     children: PropTypes.node.isRequired,
     marginright: PropTypes.string,
     justifycontent: PropTypes.string,
+    backgroundColor: PropTypes.string
 }
