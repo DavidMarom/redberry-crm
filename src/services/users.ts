09940a85bfd3f6de648c0f2457ab48097dd835bd
David Marom <davidmarom.pro@gmail.com>
2024-01-05 10:30:55 +0200

    done

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,5 +1,11 @@
 import http from '@/services/http';
 
+export function getUser(userId: string) {
+    return http.get(`users/${userId}`)
+        .then((res) => res.data)
+        .catch((error) => error);
+}
+
 export function updateUser(userId: string, user: object) {
     return http.patch(`users`, { uid: userId, ...user })
         .then((res) => res.data)
