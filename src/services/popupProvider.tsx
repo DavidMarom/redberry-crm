0ba0371950ab5035e8fc419a198c9db36e23edc9
David Marom <davidmarom.pro@gmail.com>
2024-01-10 09:44:57 +0200

    removed old popup from popup provider

--- src/services/popupProvider.tsx
+++ src/services/popupProvider.tsx
@@ -2,14 +2,14 @@
 
 import React from 'react'
 import usePopupStore from '../store/popup';
-import { EditContactPopup } from '../components/popups/EditContactPopup';
+// import { EditContactPopup } from '../components/popups/EditContactPopup';
 
 export const PopupProvider = () => {
     const popId = usePopupStore((state) => state.popId);
 
     return (
         <>
-            {(popId === 1) && <EditContactPopup />}
+            {/* {(popId === 1) && <EditContactPopup />} */}
         </>
     )
 }
