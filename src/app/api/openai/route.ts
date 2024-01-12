9e61a82a20b54999091dc13b4d4087a4f1453bdc
David Marom <davidmarom.pro@gmail.com>
2024-01-12 22:35:03 +0200

    done

--- src/app/api/openai/route.ts
+++ src/app/api/openai/route.ts
@@ -5,19 +5,17 @@ const openai = new OpenAI({ apiKey: process.env.PUBLIC_OPENAI_API_KEY });
 export async function POST(request: Request) {
     const { notesArray } = await request.json();
     console.log(notesArray);
-    const requestToOpenAI = "I own a small business, I have a few tasks, please help me prioritize them: \n" + notesArray[0] + "\n" + notesArray[1] + "\n" + notesArray[2] + "\n";
-    
-    // console.log(requestToOpenAI);
+    const requestToOpenAI = "I own a small business, I have a few tasks, please help me prioritize them: \n" + notesArray.map((note: string) => note + "\n").join("") + "\n";
 
-    // const response = await openai.chat.completions.create({
-    //     model: "gpt-3.5-turbo",
-    //     temperature: 0.1,
-    //     messages: [{ "role": "user", "content": requestToOpenAI }]
-    // });
+    const response = await openai.chat.completions.create({
+        model: "gpt-3.5-turbo",
+        temperature: 0.1,
+        messages: [{ "role": "user", "content": requestToOpenAI }]
+    });
 
     // console.log(response.choices[0].message);
 
-    return new Response(JSON.stringify('sent to openai'), {
+    return new Response(JSON.stringify(response.choices[0].message), {
         headers: { 'Content-Type': 'application/json' },
     });
 
