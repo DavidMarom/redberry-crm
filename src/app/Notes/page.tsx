c05bebe1a2b37d715cd77d39140ec66f04fd3024
David Marom <davidmarom.pro@gmail.com>
2024-06-03 07:26:50 +0300

    Made notes update faster on DOM

--- src/app/Notes/page.tsx
+++ src/app/Notes/page.tsx
@@ -13,12 +13,16 @@ const NotesPage = () => {
     const { data, isLoading, isFetching, error } = useQuery("notes", () => getNotesByOwner(uid));
     const [input, setInput] = useState("");
     const [disabled, setDisabled] = useState(false);
-
+    
     const deleteMutation = useMutation((id: string) => deleteNote(id), { onSuccess: () => { setDisabled(false); queryClient.invalidateQueries('notes') } })
     const addMutation = useMutation((note: any) => addNote(note), { onSuccess: () => { queryClient.invalidateQueries('notes') } })
     const handleChange = (event: any) => { setInput(event.target.value) }
-    const submitHandler = () => { addMutation.mutate({ text: input, owner: uid }) }
-
+    const submitHandler = () => { 
+        setDataState([...dataState, { text: input, owner: uid }]);
+        addMutation.mutate({ text: input, owner: uid }) 
+    }
+    const [dataState, setDataState] = useState(data);
+    
     return (
         <div className='full-width'>
             {isFetching || isLoading || disabled ? <h1>Loading...</h1> : <h1>Notes</h1>}
@@ -27,22 +31,18 @@ const NotesPage = () => {
                     <textarea onChange={handleChange} value={input} />
                     <div className='row-r'>
                         <button onClick={submitHandler}>
-                            <LuArrowRight style={{
-                                color: 'white',
-                                backgroundColor: '#6FC21C',
-                                width: '46px',
-                                height: '30px',
-                                borderRadius: '5px',
-                                gap: '10px',
-                                padding: '5px'
-                            }} />
+                            <LuArrowRight style={{ color: 'white', backgroundColor: '#6FC21C', width: '46px', height: '30px', borderRadius: '5px', gap: '10px', padding: '5px' }} />
                         </button>
                     </div>
                 </div>
                 {
-                    data && data.map((note: any) => (
+                    dataState && dataState.map((note: any) => (
                         <div className='grid-item' key={note._id}>
-                            <button className="row-r" onClick={() => { setDisabled(true); deleteMutation.mutate(note._id) }}><Image src="icons/trash.svg" alt="Delete" width={18} height={18} /></button>
+                            <button className="row-r" onClick={() => {
+                                setDisabled(true);
+                                setDataState(dataState.filter((item: any) => item._id != note._id));
+                                deleteMutation.mutate(note._id)
+                            }}><Image src="icons/trash.svg" alt="Delete" width={18} height={18} /></button>
                             {deleteMutation.isError && <div>Something went wrong</div>}
                             <div>{note.text}</div>
                         </div>
