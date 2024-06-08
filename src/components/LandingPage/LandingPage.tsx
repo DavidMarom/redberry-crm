25b521f0c8abcec52ad164b176a24085c3e2c6e8
David Marom <davidmarom@hotmail.com>
2024-06-08 18:34:15 +0300

    added onMutate

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,8 +1,4 @@
-"use client";
-
 import React from 'react'
-import PropTypes from "prop-types"
-import { StyleSheetManager } from 'styled-components';
 import { Strip01, BulletRow, Hero, HeroText } from './LandingPage.style.js';
 import Image from 'next/image';
 import { Btn } from "@/components";
@@ -11,7 +7,6 @@ import { signupHandler } from "@/utils/userUtils";
 export default function LandingPage({ }) {
 
     return (
-        // <StyleSheetManager shouldForwardProp={(prop) => prop !== 'margintop' || prop !== 'marginright'}>
         <Strip01>
             <div>
                 <h1>Lightweight CRM</h1>
@@ -37,7 +32,6 @@ export default function LandingPage({ }) {
             </div>
             <Hero><Image src="icons/Hero.svg" alt="strip01" width={760} height={100} priority /></Hero>
         </Strip01>
-        // </StyleSheetManager>
     )
 }
 
