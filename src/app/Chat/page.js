85e3de51c531201d04d141d78c936a86e8de3dd2
David Marom <davidmarom@hotmail.com>
2024-06-19 19:01:42 +0300

    added ec2 address

--- src/app/Chat/page.js
+++ src/app/Chat/page.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
 // const socket = io("http://localhost:3001",
-const socket = io("http://ec2-3-66-18-219.eu-central-1.compute.amazonaws.com:3001",
+const socket = io("ec2-18-153-81-26.eu-central-1.compute.amazonaws.com:80",
 
 
   {
