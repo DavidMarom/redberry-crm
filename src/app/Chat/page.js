6933f4828d50483f60debb91d7f288f0fe28550d
David Marom <davidmarom@hotmail.com>
2024-06-20 12:19:34 +0300

    not working yet

--- src/app/Chat/page.js
+++ src/app/Chat/page.js
@@ -5,7 +5,7 @@ import io from "socket.io-client";
 
 // const socket = io("http://localhost:3001",
 // const socket = io("ec2-18-153-81-26.eu-central-1.compute.amazonaws.com:80",
-const socket = io("ec2-3-72-14-4.eu-central-1.compute.amazonaws.com",
+const socket = io("http://ec2-3-72-14-4.eu-central-1.compute.amazonaws.com:80",
 
 
   {
