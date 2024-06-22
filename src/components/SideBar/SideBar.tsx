f0b3118f880a20c926ec6fde89c4c54932e79e8d
David Marom <davidmarom@hotmail.com>
2024-06-22 15:23:16 +0300

    Added Chat (#199)
    * working
    
    * isAlive
    
    * added ec2 address
    
    * changed address
    
    * wip
    
    * David/examples (#192)
    
    * config example
    
    * added example files
    
    * David/chat (#193)
    
    * not working yet
    
    * wip
    
    * David/chat2 (#196)
    
    * not working yet
    
    * wip
    
    * works
    
    * wip
    
    * wip
    
    * David/chat3 (#197)
    
    * not working yet
    
    * wip
    
    * works
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * David/chat4 (#198)
    
    * not working yet
    
    * wip
    
    * works
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -2,6 +2,9 @@
 import Link from "next/link";
 import styles from "./sidebar.module.css";
 import Image from 'next/image';
+import { BsChatLeftText } from "react-icons/bs";
+
+
 function SideBar() {
   return (
     <>
@@ -9,48 +12,47 @@ function SideBar() {
         <div>
           <Link href="/">
             <div className={styles.sidebarbtn}>
-              <div className={styles.sidebarbtnwrapper}>
-                <Image src="icons/dashboard-w.svg" alt="dashboard" width={20} height={20}></Image>
-                <span>Overview</span>
-              </div>
+              <div className={styles.icon}><Image src="icons/dashboard-w.svg" alt="dashboard" width={20} height={20}></Image></div>
+              <p>Overview</p>
             </div>
           </Link>
           <Link href="/Contacts">
             <div className={styles.sidebarbtn}>
-              <div className={styles.sidebarbtnwrapper}>
-                <img src="icons/contacts-w.svg" alt="contacts" width={20} />
-                <span style={{paddingRight: '8px'}}>Contacts</span>
-              </div>
+              <div className={styles.icon}><img src="icons/contacts-w.svg" alt="contacts" width={20} /></div>
+              <p>Contacts</p>
             </div>
           </Link>
           <Link href="/Notes">
             <div className={styles.sidebarbtn}>
-              <div className={styles.sidebarbtnwrapper}>
-                <img src="icons/notes-w.svg" alt="notes" width={20} />
-                <span className={styles.sidebartxt}>Notes</span>
-              </div>
+              <div className={styles.icon}><img src="icons/notes-w.svg" alt="notes" width={20} /></div>
+              <p style={{ paddingRight: '8px' }}>Notes</p>
             </div>
           </Link>
           <Link href="/Email">
             <div className={styles.sidebarbtn}>
-              <div className={styles.sidebarbtnwrapper}>
-                <img src="icons/mail-w.svg" alt="mail" width={20} />
-                <span className={styles.sidebartxt}>Email</span>
-              </div>
+              <div className={styles.icon2}><img src="icons/mail-w.svg" alt="mail" width={20} /></div>
+              <p>Email</p>
             </div>
           </Link>
+
         </div>
         <div>
+          <Link href="/Chat">
+            <div className={styles.sidebarbtn}>
+              <div className={styles.icon2}><BsChatLeftText size={20} /></div>
+              <p>Chat</p>
+            </div>
+          </Link>
+
           <Link href="/Settings">
             <div className={styles.sidebarbtn}>
-              <div className={styles.sidebarbtnwrapper}>
-                <img src="icons/cog-w.svg" alt="settings" width={20} />
-                <span className={styles.sidebartxt}>Settings</span>
-              </div>
+              <div className={styles.icon2}><img src="icons/cog-w.svg" alt="settings" width={20} /></div>
+              <p>Settings</p>
             </div>
           </Link>
         </div>
       </div>
+
       <div className={styles.containermobile}>
         <div>
           <Link href="/">
@@ -73,7 +75,14 @@ function SideBar() {
               <img src="icons/mail-w.svg" alt="mail" width={20} />
             </div>
           </Link>
+          <br />
+          <Link href="/Chat">
+            <div className={styles.sidebarbtn}>
+              <BsChatLeftText size={20} />
+            </div>
+          </Link>
         </div>
+
         <div>
           <Link href="/Settings">
             <div className={styles.sidebarbtn}>
