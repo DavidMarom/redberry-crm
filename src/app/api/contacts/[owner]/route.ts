8bb55611eba75aff3802cbf5b0c670b665c4c225
David Marom <davidmarom@hotmail.com>
2025-12-13 11:52:29 +0200

    fix

--- src/app/api/contacts/[owner]/route.ts
+++ src/app/api/contacts/[owner]/route.ts
@@ -1,14 +1,20 @@
 import { connectDatabase, getDocumentsByFilter } from "../../../../services/mongo";
-import { ContactGetType } from "../../apiTypes";
 
-export async function GET(request: Request, params: ContactGetType) {
-    const owner = params.params.owner;
+export async function GET(
+    request: Request,
+    { params }: { params: { owner: string } }
+) {
+    const owner = params.owner;
+
     const client = await connectDatabase();
-    const documents = await getDocumentsByFilter(client, 'contacts', { owner: owner });
+    const documents = await getDocumentsByFilter(
+        client,
+        "contacts",
+        { owner }
+    );
     client.close();
+
     return new Response(JSON.stringify(documents), {
-        headers: { 'Content-Type': 'application/json' },
+        headers: { "Content-Type": "application/json" },
     });
 }
-
-
